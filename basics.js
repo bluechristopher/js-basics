1edd02be3abbd02c10b4c09da984d210200d86d4
bluechristopher <139408980+bluechristopher@users.noreply.github.com>
2024-07-10 09:10:01 +0800

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,1 +1,27 @@
+let myName = "Mr Lam";
+const myAge = 18;  // constant, cannot be changed
+let myPhone = 1234567;
+myPhone = 7654321;
+let myArr = [1, 2, 3, 4, 5, 6];
+let isHuman = true;
+
+console.log(myName);
+console.log(myAge);
+console.log(myPhone);
+console.log(myArr);
+console.log(isHuman);
+
+// Playing with numbers
+let a = (5 + 7) * 2;
+let b = (5 - 7) / 3;
+let c = 5 ** 7; // power
+let d = 123 % 45;  // remainder
+console.log(a, b, c, d);
+
+let e = Math.random();
+let f = Math.round(123.45678);
+let g = Math.cos(Math.PI);
+let h = Math.E ** 2;
+console.log(e, f, g, h);
+
 
